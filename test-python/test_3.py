8f039ca9874e413cd053477eefe3626d8c433db7
softpymesFelipe <lmelo@softpymes.com.co>
2022-02-12 09:42:45 -0500

    Update test_3.py

--- test-python/test_3.py
+++ test-python/test_3.py
@@ -1,7 +1,9 @@
-
-# 3) ordenar los terceros que se tienen en el archivo data.py 
-# por nombre, para obtener el nombre correcto se debe tener en 
-# cuenta la siguiente validación:
-#  si el tercero tiene un tradename != '' entonces se muestra este valor, 
-# en caso contrario se debe obtener concatenando los siguientes 
-# campos: (firstname, lastname, maidenname) en el orden dado
+"""
+  3) ordenar los terceros que se tienen en el archivo data.py 
+  por nombre, para obtener el nombre correcto se debe tener en 
+  cuenta la siguiente validación:
+  
+  si el tercero tiene un (radename != '') entonces se muestra este valor, 
+  en caso contrario se debe obtener concatenando los siguientes 
+  campos: (firstname, lastname, maidenname) en el orden dado
+"""
