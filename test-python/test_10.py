587e57164cce63e39a5380da5477420953d33334
softpymesFelipe <lmelo@softpymes.com.co>
2022-02-12 09:48:12 -0500

    Update test_10.py

--- test-python/test_10.py
+++ test-python/test_10.py
@@ -1,3 +1,5 @@
-# 10) realice una consulta al archivo data.py, muestre todos los terceros, reduzca la 
-# información y solo muestre el nombre y la identificación, 
-# ordenelos por identificación
+"""
+  10) realice una consulta al archivo data.py, muestre todos los terceros, reduzca la 
+  información y solo muestre el nombre y la identificación, 
+  ordenelos por identificación
+"""
