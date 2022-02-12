3d4e0902146df365a5a86d4f6d90cea0a3b97723
softpymesFelipe <lmelo@softpymes.com.co>
2022-02-12 09:40:51 -0500

    Update test_1.py

--- test-python/test_1.py
+++ test-python/test_1.py
@@ -1,6 +1,6 @@
-
-
-# 1) consulte la información del archivo data.py
-# cree un objeto que contenga las empresas y dentro 
-# las sucursales que corresponden para cada empresa
+""" 
+  1) consulte la información del archivo data.py
+  cree un objeto que contenga las empresas y dentro 
+  las sucursales que corresponden para cada empresa
+"""
 
