8ce819c5aca716f311de59496e65b32809d155b9
softpymesFelipe <lmelo@softpymes.com.co>
2022-02-12 09:45:34 -0500

    Update test_7.py

--- test-python/test_7.py
+++ test-python/test_7.py
@@ -1,4 +1,4 @@
-
-
-# 7) realice una consulta al archivo data.py, muestre los items que no tienen colores 
-# y ordenelos por nombre
+"""
+  7) realice una consulta al archivo data.py, muestre los items que no tienen colores 
+  y ordenelos por nombre
+"""
