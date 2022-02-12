5b491479a825a3cd5b478577a812da7e1036eada
softpymesFelipe <lmelo@softpymes.com.co>
2022-02-12 09:44:24 -0500

    Update test_5.py

--- test-python/test_5.py
+++ test-python/test_5.py
@@ -1,3 +1,4 @@
-
-# 5) ordenar los terceros que se tienen en el archivo data.py por nombre y 
-# agregar dentro de cada tercero la compañia a la que pertenece
+"""
+  5) ordenar los terceros que se tienen en el archivo data.py por nombre y 
+  agregar dentro de cada tercero una propiedad que muestre la compañia a la que pertenece
+"""
