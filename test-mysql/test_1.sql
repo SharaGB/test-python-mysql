fc63eaa9c3a93e3d4f461a85d4fde68b08e641c9
softpymesFelipe <lmelo@softpymes.com.co>
2022-02-12 09:31:59 -0500

    Update test_1.sql

--- test-mysql/test_1.sql
+++ test-mysql/test_1.sql
@@ -1,13 +1,13 @@
 # Generar scripts que realicen las siguientes consultas
 
--------*Consultar los items de la compañia (utilizando INNER JOIN) #3*-------
+-------* Consultar los items que pertenezcan a la compañia (utilizando INNER JOIN) con ID #3 *-------
 
 
--------*Mostrar los últimos 10 items en general*-------
+-------* Mostrar los últimos 10 items *-------
 
 
--------*Mostrar los items que en el nombre terminen con la A*-------
+-------* Mostrar los items que en el nombre terminen con la letra A *-------
 
 
--------*Mostrar los items que tengan relacionado el color Rojo*-------
+-------* Mostrar los items que tengan relacionado el color Rojo *-------
 
