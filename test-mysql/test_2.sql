2204060f3bac6b7c8c58077cc0d8046833b702e2
softpymesFelipe <lmelo@softpymes.com.co>
2022-02-12 09:33:25 -0500

    Update test_2.sql

--- test-mysql/test_2.sql
+++ test-mysql/test_2.sql
@@ -1,10 +1,10 @@
 # Generar scripts que realicen las siguientes modificaciones
 
--------*Colocarle el Precio a los items que lo tengan en NULL, tomando como referencia el costo más 10.000*-------
+-------* Colocarle el Precio a los items que lo tengan en NULL, tomando como referencia el valor del costo del item + 10.000 *-------
 
 
--------*Incrementar el precio de los items un 10%*-------
+-------* Incrementar el precio de los items en 10% *-------
 
 
--------*Anteponer la palabra Nuevo a los items que comiencen con C en el nombre*-------
+-------* Anteponer la palabra Nuevo a los items que comiencen con C en el nombre *-------
 
